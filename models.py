fbe37423c5158cc9c57bf5a8333382b5dacbae89
Contemplat0r <uldo.forme@gmail.com>
2019-03-30 20:30:01 +0200

    Exps with MLP and clustering continue

---
+++ models.py
@@ -0,0 +1,28 @@
+#seequential_nn_model_relu_droput01_lr001_sigmoid_40_epochs_2019-03-30-1
+sequential_nn_model = None
+#del sequential_nn_model
+if sequential_nn_model:
+    del sequential_nn_model
+sequential_nn_model = Sequential()
+sequential_nn_model.add(Dense(batch_size, input_dim=200, kernel_initializer='normal', activation='relu'))
+#sequential_nn_model.add(Dense(batch_size, input_shape=(100, 200), kernel_initializer='normal', activation='sigmoid'))
+#sequential_nn_model.add(Dropout(0.76))
+sequential_nn_model.add(Dropout(0.1))
+sequential_nn_model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+
+sequential_nn_model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy', auc])
+
+
+#sequential_nn_model_relu_droput024_lr001_sigmoid_40_epochs_2019-03-30-1
+sequential_nn_model = None
+#del sequential_nn_model
+if sequential_nn_model:
+    del sequential_nn_model
+sequential_nn_model = Sequential()
+sequential_nn_model.add(Dense(batch_size, input_dim=200, kernel_initializer='normal', activation='relu'))
+#sequential_nn_model.add(Dense(batch_size, input_shape=(100, 200), kernel_initializer='normal', activation='sigmoid'))
+#sequential_nn_model.add(Dropout(0.76))
+sequential_nn_model.add(Dropout(0.24))
+sequential_nn_model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+
+sequential_nn_model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy', auc])
